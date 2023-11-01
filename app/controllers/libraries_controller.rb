7358655862b10494fc728e08eab93214cd3573f3
cameron oakes <cameronoakes@outlook.com>
2023-11-01 17:34:44 +0000

    working image upload

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,4 +1,6 @@
 class LibrariesController < ApplicationController
+  before_action :set_user
+
   def new
     @library_new = Library.new
     @user = User.find(current_user.id)
@@ -6,10 +8,15 @@ class LibrariesController < ApplicationController
 
   def create
     @library_new = Library.new(library_params)
+    @library_new.user = @user
 
     if @library_new.save
       redirect_to @library_new, notice: 'Library created'
     else
+      puts " -----------
+      #{@library_new.errors}
+      --------------
+      "
       render :new
     end
   end
@@ -19,12 +26,16 @@ class LibrariesController < ApplicationController
   end
 
   def show
-    @library = Library.find(params[:query])
+    @library = Library.find(params[:id])
   end
 
   private
 
   def library_params
-    params.require(:library).permit(:name, :description, :photos)
+    params.require(:library).permit(:name, :description, photos: [])
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
