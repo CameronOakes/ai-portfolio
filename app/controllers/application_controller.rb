80c27f83793716de8f1a8f84645a893c41d3c3a7
cameron oakes <cameronoakes@outlook.com>
2024-01-06 17:23:04 +0000

    avatar changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :avatar, :email, :password) }
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :username, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :username, :avatar, :password, :current_password) }
   end
 end
