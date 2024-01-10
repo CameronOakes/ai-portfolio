e7b028a6f65aada0268d994eb2086a128c6cfef6
cameron oakes <cameronoakes@outlook.com>
2024-01-10 17:38:20 +0000

    splide and index page changes

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,8 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
+import Splide from '@splidejs/splide';
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
+  static targets = [ "carousel" ]
+
   connect() {
     console.log("Hello, Stimulus!", this.element)
+    console.log(this.carouselTarget)
+    new Splide( this.carouselTarget, {
+      type: 'loop',
+      perPage: 1, // Change perPage to 1 to show full image
+      perMove: 1,
+      gap: '1rem',
+      drag: 'free',
+      pagination: false,
+      fixedWidth: '100%', // Set the width of the image
+      center: true, // Center the image
+      breakpoints: {
+        640: {
+          perPage: 1,
+        },
+        768: {
+          perPage: 1,
+        },
+        1024: {
+          perPage: 1,
+        },
+      }
+    }).mount();
   }
 }
+// TODO: fix carousel so it starts in the center of the screen
