4579f053ea2ed2f2d9f1d80fc68138f9fe92a974
cameron oakes <cameronoakes@outlook.com>
2024-04-15 20:09:54 +0100

    splide not working pls help

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -3,32 +3,30 @@ import Splide from '@splidejs/splide';
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
-  static targets = [ "carousel" ]
+  static targets = [ "carousel", "thumbnail"]
 
   connect() {
     console.log("Hello, Stimulus!", this.element)
     console.log(this.carouselTarget)
-    new Splide( this.carouselTarget, {
-      type: 'loop',
-      perPage: 1,
-      perMove: 1,
-      gap: '5rem',
-      drag: 'free',
-      pagination: true,
-      heightRatio: 0.5, // Set the height ratio to 0.5 for half the screen height
-      autoWidth: true, // Set autoWidth to true to make the slides have the same width
-      breakpoints: {
-        640: {
-          perPage: 1,
-        },
-        768: {
-          perPage: 1,
-        },
-        1024: {
-          perPage: 1,
-        },
-      }
-    }).mount();
+    var splide = new Splide( this.carouselTarget, {
+      focus: 'center',
+    });
+
+    splide.mount();
+
+    var thumbnails = document.getElementsByClassName( 'thumbnail' );
+
+    for ( var i = 0; i < thumbnails.length; i++ ) {
+      initThumbnail( thumbnails[ i ], i );
+    }
+
+    function initThumbnail( thumbnail, index ) {
+      thumbnail.addEventListener( 'click', function () {
+        splide.go( index );
+      } );
+    }
   }
+
+
 }
 // TODO: fix carousel so it starts in the center of the screen
