e647fe1979daff10b2909e3df85ed25b61c21e1c
cameron oakes <cameronoakes@outlook.com>
2024-05-15 14:23:13 +0100

    sidebar toggle

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -2,11 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sidebar"
 export default class extends Controller {
-  static targets = [ "sidebar" ]
+  static targets = [ "sidebar", "openSidebarButton", "closeSidebarButton" ]
 
   connect() {
     console.log("Hello, Stimulus! from sidebar", this.element)
     window.addEventListener("scroll", this.scrollHandler.bind(this));
+    const sidebar = this.sidebarTarget;
+    sidebar.style.display = 'none';
+    const openSidebarButton = this.openSidebarButtonTarget;
+    const closeSidebarButton = this.closeSidebarButtonTarget;
+
+
+    openSidebarButton.addEventListener('click', () => {
+      sidebar.style.display = 'block';
+      openSidebarButton.style.display = 'none';
+    });
+
+    closeSidebarButton.addEventListener('click', () => {
+      sidebar.style.display = 'none';
+      openSidebarButton.style.display = 'block';
+    });
   }
 
   disconnect() {
